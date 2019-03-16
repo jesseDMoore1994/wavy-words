68babdb54d0371c3193ce9e7379c80e932de5861
Pete Dietl <pdietl3@gatech.edu>
2019-03-15 20:08:20 -0500

    Fix print out behavior when the terminal size is changed or program is interrupted

--- wavy-words.py
+++ wavy-words.py
@@ -1,16 +1,27 @@
-import sys, time
-import os
-import math
+import sys, time, os, math, signal
+
+def sigint(sig, frame):
+    print("\nAll Done! :p")
+    exit(0)
+
+def sigwinch(sig, frame):
+    wave.setup()
 
 class SinWordWave:
     def __init__(self, input_string, period):
-        rows, columns = os.popen('stty size', 'r').read().split()
         self.str = input_string
-        self.min_offset = 0
-        self.max_offset = int(columns)-len(self.str)
-        self.amplitude = round(self.max_offset / 2) - 1
-        self.period = period
         self.current_degree = 0
+        self.period = period
+        self.setup()
+
+    def setup(self):
+        try:
+            columns = os.get_terminal_size(0).columns
+        except OSError:
+            columns = os.get_terminal_size(1).columns
+        self.min_offset = 0
+        self.max_offset = int(columns) - len(self.str)
+        self.amplitude = round(self.max_offset / 2) - 1
         self.offset = self.calculate_offset()
 
     def calculate_offset(self):
@@ -24,11 +35,12 @@ class SinWordWave:
             print(' ' * self.offset + self.str, flush=True)
             self.offset = self.calculate_offset()
 
-
 if __name__ == "__main__":
-    try:
-        wave = SinWordWave(sys.argv[1], None)
-    except IndexError:
+    signal.signal(signal.SIGINT, sigint)
+    signal.signal(signal.SIGWINCH, sigwinch)
+    if len(sys.argv) < 2:
         print("You need to pass in a string for the wave at least...")
+        exit(0)
 
+    wave = SinWordWave(sys.argv[1], None)
     wave.start_the_wave()
